4a8471bf8d2f3a28a82b574160ecc0fbf3e9f4cd
xayalahmad <khayall.ahmadli@gmail.com>
2024-02-16 12:06:00 +0400

    style(TASK-2): update responsive design

--- src/components/main/ExamCategory/index.tsx
+++ src/components/main/ExamCategory/index.tsx
@@ -1,3 +1,4 @@
+import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 export default function ExamCategory() {
     return (
@@ -9,32 +10,77 @@ export default function ExamCategory() {
                         Kateqoriya seç
                     </div>
                 </div>
-                <div className="flex flex-wrap gap-4     mt-10 justify-center md:justify-center">
-                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center w-44 whitespace-nowrap text-base font-semibold rounded-xl">
+                <Box className='mt-10'>
+                    <Grid container spacing={3} className=''>
+                        <Grid item xs={6} sm={4} xl={3}>
+                            <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
+                                Buraxılış
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6} sm={4} xl={3}>
+                            <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
+                                Blok
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6} sm={4} xl={3}>
+                            <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
+                                Təkmilləşdirmə
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6} sm={4} xl={3}>
+                            <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
+                                İbtidai sinif
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6} sm={4} xl={3}>
+                            <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
+                                Liseylərə
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6} sm={4} xl={3}>
+                            <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
+                                Magistratura
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6} sm={4} xl={3}>
+                            <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
+                                Olimpiadalar
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6} sm={4} xl={3}>
+                            <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
+                                Fənn imtahanı
+                            </Box>
+                        </Grid>
+
+                    </Grid>
+                </Box>
+                {/* <div className="flex flex-wrap gap-4     mt-10 justify-center md:justify-center">
+                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
                         Buraxılış
                     </Box>
-                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center w-44 whitespace-nowrap text-base font-semibold rounded-xl">
+                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
                         Blok
                     </Box>
-                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center w-44 whitespace-nowrap text-base font-semibold rounded-xl">
+                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
                         Təkmilləşdirmə
                     </Box>
-                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center w-44 whitespace-nowrap text-base font-semibold rounded-xl">
+                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
                         İbtidai sinif
                     </Box>
-                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center w-44 whitespace-nowrap text-base font-semibold rounded-xl">
+                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
                         Liseylərə
                     </Box>
-                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center w-44 whitespace-nowrap text-base font-semibold rounded-xl">
+                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
                         Magistratura
                     </Box>
-                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center w-44 whitespace-nowrap text-base font-semibold rounded-xl">
+                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
                         Olimpiadalar
                     </Box>
-                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center w-44 whitespace-nowrap text-base font-semibold rounded-xl">
+                    <Box sx={{ backgroundColor: '#fafafa', boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.2)' }} className=" h-24 flex items-center justify-center min-w-32 whitespace-nowrap text-base font-semibold rounded-xl">
                         Fənn imtahanı
                     </Box>
-                </div>
+                </div> */}
             </div>
         </>
     )
